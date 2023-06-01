534a0f78f5ff48e0cd8abdd8dedb38d9ce13f951
Oleksandr Saprykin <vxlam7@gmail.com>
2023-06-01 18:55:01 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApp_course_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
